20514f213f5e7eacc6419dca2d5fd0aad4cfaa92
ehawk823 <evan.alexander.hawk@gmail.com>
2015-08-05 09:58:28 -0500

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,59 @@
-# Write code here
+class School
+  
+  @@roster = {}
+  @@ninth_grade = []
+  @@tenth_grade = []
+  @@seventh_grade = []
+
+  def initialize (school_name)
+    @school_name = school_name
+    @ninth_grade = []
+    @tenth_grade = []
+    @seventh_grade = []
+  end
+
+  def add_student(name, grade)
+    if grade == 7
+      seventh_grade << name
+      @@roster[grade] = seventh_grade
+    end
+    if grade == 9
+      ninth_grade << name
+      @@roster[grade] = ninth_grade
+    end
+    if grade == 10
+      tenth_grade << name
+      @@roster[grade] = tenth_grade
+    end
+  end
+
+  def roster
+    @@roster
+  end
+
+  def seventh_grade
+    @seventh_grade
+  end
+
+  def ninth_grade
+    @ninth_grade
+  end
+
+  def tenth_grade
+    @tenth_grade
+  end
+
+  def grade (grade)
+    @@roster[grade]
+  end
+
+  def sort
+    @@roster[7] = @seventh_grade.sort
+    @@roster[9] = @ninth_grade.sort
+    @@roster[10] = @tenth_grade.sort
+    @@roster
+  end
+end
+
+
+
